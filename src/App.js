9058c6c09a33052fb94734350426758b89cd0d49
Rei Krom <mr.mygo@gmail.com>
2020-05-24 16:37:57 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import { encouragements } from "./encouragement";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [number, setNumber] = useState(1);
+
+	const chooseEncouragement = () => {
+		setNumber(Math.floor(Math.random() * Math.floor(101)));
+	};
+
+	useEffect(() => {
+		setInterval(() => {
+			chooseEncouragement();
+		}, 30000);
+		return () => {
+			clearInterval();
+		};
+	}, []);
+
+	return (
+		<div className="App">
+			<header onClick={chooseEncouragement} className="App-header">
+				<h1 style={{ color: "white" }}>{encouragements[number]}</h1>
+			</header>
+		</div>
+	);
 }
 
 export default App;
